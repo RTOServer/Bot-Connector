add22737d828d23076f03a460c66f766cc3b89b7
Cjsah <cjsah@cjsah.net>
2024-11-01 16:45:35 +0800

    update color

--- src/main/java/net/cjsah/rtos/bot/http/RTOSHttpServer.java
+++ src/main/java/net/cjsah/rtos/bot/http/RTOSHttpServer.java
@@ -52,7 +52,7 @@ public class RTOSHttpServer {
             }
 
             String msg = "<%s> %s".formatted(body.player(), body.msg());
-            MutableText text = Text.literal(msg).formatted(Formatting.DARK_GRAY);
+            MutableText text = Text.literal(msg).formatted(Formatting.GRAY);
             this.server.getPlayerManager().broadcast(text, false);
             this.server.sendMessage(text);
 
